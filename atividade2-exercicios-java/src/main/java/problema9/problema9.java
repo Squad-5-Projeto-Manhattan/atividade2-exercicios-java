1c8be31006eb0f1a5d722b8cfd0d93e8a060a04b
Thalison <Tec.thalison@gmail.com>
2023-12-30 03:27:03 -0300

    Inciando Problema 9

--- atividade2-exercicios-java/src/main/java/problema9/problema9.java
+++ atividade2-exercicios-java/src/main/java/problema9/problema9.java
@@ -1,10 +1,29 @@
 package problema9;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class problema9 {
 
     public static void main(String[] args) {
+        //
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Informe quantidade de números inteiros");
+        int qtdInformada = scanner.nextInt();
+
+        int [] vetor = new int[qtdInformada];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+
+            System.out.println(Arrays.stream(vetor).iterator());
+
+        }
 
     }
 }
