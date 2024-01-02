d395634e4329ed5153166978fb8e8e8f39f0046b
Igor Farias <balestfarias@gmail.com>
2024-01-02 12:10:47 -0300

    renomeacao-pacotes

--- atividade2-exercicios-java/src/main/java/igor/iproblema26/IProblema26.java
+++ atividade2-exercicios-java/src/main/java/igor/iproblema26/IProblema26.java
@@ -1,11 +1,11 @@
-package igor.problema26;
+package igor.iproblema26;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Problema26 {
+public class IProblema26 {
 
     // declarando as filas
     static Queue<Integer> fila;
