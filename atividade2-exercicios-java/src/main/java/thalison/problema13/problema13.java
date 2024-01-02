a8aaa8330d0e3d42f62f8f91c72fdd0b0fb21097
Thalison <Tec.thalison@gmail.com>
2024-01-02 06:56:34 -0300

    Resolução problema 13 e 15

--- atividade2-exercicios-java/src/main/java/thalison/problema13/problema13.java
+++ atividade2-exercicios-java/src/main/java/thalison/problema13/problema13.java
@@ -1,8 +1,27 @@
 package thalison.problema13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class problema13 {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numeroInformado, verificaEnquanto, somaNumerosImpares = 0;
+        List<Integer> listaNumeros  = new ArrayList<>();
+
+
+
+        //Percorre a lista e o "i" é o elemento "valor" que está na lista
+        for (Integer i : listaNumeros){
+            if (!(i % 2 ==0)){ //Usado primeiramente encontrar o par e utilizado a negação para pegar os ímpares
+                somaNumerosImpares += i;
+            }
+        }
+
+        System.out.print("A soma dos números ímpares é: " + somaNumerosImpares);
 
     }
 }
---
+++ atividade2-exercicios-java/src/main/java/thalison/problema15/problema15.java
@@ -0,0 +1,63 @@
+package thalison.problema15;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+import java.util.Stack;
+
+import java.util.Stack;
+
+public class problema15 {
+    public static void main(String[] args) {
+        Stack<Integer> pilha = new Stack<>();
+        int numeroInformado, verificaEnquanto, somaNumerosImpares = 0;
+        Scanner scanner = new Scanner(System.in);
+        // Adicionando elementos à pilha
+        do {
+
+            System.out.println("Informe um número:");
+            numeroInformado = scanner.nextInt();
+           pilha.push(numeroInformado);
+
+            System.out.println("Deseja adicionar outro número a lista digite:  1 - sim ou qualquer outro número para sair");
+            verificaEnquanto = scanner.nextInt();
+
+        }while (verificaEnquanto == 1);
+
+        System.out.println("Pilha original: " + pilha);
+
+        inverterPilha(pilha);
+
+        System.out.println("Pilha invertida: " + pilha);
+    }
+
+    //Metodo que recbe a pilha de inteiros e realiza a inversão;
+    public static void inverterPilha(Stack<Integer> pilha) {
+        if (!pilha.isEmpty()) {
+            // Retire o elemento do topo
+            int elemento = pilha.pop();
+
+            // Inverta a ordem dos elementos restantes
+            inverterPilha(pilha);
+
+            // Insira o elemento no fundo da pilha
+            inserirNoFundo(pilha, elemento);
+        }
+    }
+
+
+    public static void inserirNoFundo(Stack<Integer> pilha, int elemento) {
+        if (pilha.isEmpty()) {
+            pilha.push(elemento);
+        } else {
+            // Retire o elemento do topo
+            int topo = pilha.pop();
+
+            // Insira o elemento no fundo
+            inserirNoFundo(pilha, elemento);
+
+            // Empurre de volta o elemento original
+            pilha.push(topo);
+        }
+    }
+}
